b0c336ce5d442b9f53cefc57cd062ac9a3a0b600
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-12 16:56:49 -0300

    feat(): Criando schemas e logger

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/paula-michele-brisa/API-Gopportunities/router"
+import (
+	"github.com/paula-michele-brisa/API-Gopportunities/config"
+	"github.com/paula-michele-brisa/API-Gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Inicializar Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization erro: %v", err)
+		return
+	}
+
 	// Inicializar Router
 	router.Initialize()
 }
