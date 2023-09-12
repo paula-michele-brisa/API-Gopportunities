ab444d597da722045f3d0b7934822cd1d7c5af51
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-12 15:54:28 -0300

    feat: Criando logica de rotas

--- router/router.go
+++ router/router.go
@@ -5,11 +5,10 @@ import "github.com/gin-gonic/gin"
 // Initialize inicializa o router utilizando as configurações Default do gin
 func Initialize() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(r)
+
+	// Run the server
 	r.Run()
 	// listen and serve on 0.0.0.0:8080
 }
