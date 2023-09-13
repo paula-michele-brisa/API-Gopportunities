90a85ec5a5d25fe59be351376af6564324040be2
Paula Micheli <paula.michele@grupobrisanet.com.br>
2023-09-13 09:51:22 -0300

    feat(): Criando database

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"errors"
+	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -11,7 +11,21 @@ var (
 )
 
 func Init() error {
-	return errors.New("fake Error")
+	var err error
+
+	// Initialize SQLite
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initalize sqlite %v", err)
+	}
+
+	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
+
 }
 
 func GetLogger(p string) *Logger {
